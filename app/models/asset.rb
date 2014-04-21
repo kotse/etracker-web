24deb2c38174e7b5ae8a6369d6c5c943d0601062
Kostadin Golev <kotseto@gmail.com>
2014-04-22 00:50:44 +0300

    added validation and improved tests

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,2 +1,5 @@
 class Asset < ActiveRecord::Base
+	validates :user_id, presence: true
+	validates :asset_type, presence: true
+	validates :amount, presence: true
 end
--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -12,4 +12,24 @@ describe Asset do
   it { should respond_to(:description) }
   it { should respond_to(:amount) }
   it { should respond_to(:user_id) }
+
+  it { should be_valid }
+
+  describe "when user_id is not present" do
+    before { @asset.user_id = nil }
+
+    it { should_not be_valid }
+  end
+
+  describe "when asset_type is not present" do
+    before { @asset.asset_type = nil }
+    
+    it { should_not be_valid }
+  end
+
+  describe "when amount is not present" do
+    before { @asset.amount = nil }
+    
+    it { should_not be_valid }
+  end
 end
