9ce88ffae962ccaaaaf0769353676ef0321dc9c4
Kostadin Golev <kotseto@gmail.com>
2014-04-23 23:32:00 +0300

    asset controller added

---
+++ app/controllers/asset_controller.rb
@@ -0,0 +1,13 @@
+class AssetController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 ETracker::Application.routes.draw do
+  get 'asset/index'
+
+  get 'asset/create'
+
+  get 'asset/destroy'
+
+  get 'asset/edit'
+
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
