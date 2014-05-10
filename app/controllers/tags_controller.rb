ed3740d5af8c3b0aa317274308c84c4c4c3b836c
Kostadin Golev <kotseto@gmail.com>
2014-05-10 17:26:15 +0300

    a very simple tags view added

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,47 @@
+class TagsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+  	@tags = current_user.tags.all
+  end
+
+  def new
+  	@tag  = current_user.tags.build
+  end
+
+  def create
+  	@tag  = current_user.tags.build (tag_params)
+  	if @tag.save
+      flash[:success] = "Tag added succesfully!"
+      redirect_to tags_path
+    else
+      flash[:error] = "Tag not added succesfully!"
+      render 'tags/index'
+    end
+  end
+
+  def destroy
+  	tag = Tag.find(params[:id])
+    tag.destroy
+    redirect_to tags_path, :notice => "Tag deleted."
+  end
+
+  def edit
+  	@tag = Tag.find(params[:id])
+  end
+
+  def update
+  	@tag = Tag.find(params[:id])
+    if @tag.update_attributes(tag_params)
+      redirect_to tags_path, :notice => "Changes saved."
+    else
+      redirect_to tags_path, :alert => "Unable to save changes."
+    end
+  end
+
+  private
+
+    def tag_params
+      params.require(:tag).permit(:name, :color)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 ETracker::Application.routes.draw do
-  get 'expenses/index'
 
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
   resources :assets
   resources :expenses
+  resources :tags
 end
